1fc7ccf36e8d5f06b3d32645f96995a98d50364a
Nephtali Ceballos <elegy0101@gmail.com>
2022-11-05 17:40:27 -0600

    Adds migrations and run migrate for github actions

---
+++ src/database/config.js
@@ -0,0 +1,7 @@
+module.exports = {
+    username: process.env.MYSQL_USER || 'admin',
+    password: process.env.MYSQL_PASSWORD || 'admin',
+    database: 'training',
+    host: process.env.MYSQL_HOST || '127.0.0.1',
+    dialect: 'mysql'
+}
